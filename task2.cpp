2fb2b53866b20ec13ecca02832bc162378aa7db2
Harmain Iftikhar <120156793+harman-dev1@users.noreply.github.com>
2023-01-10 14:48:11 +0500

    In this C++ program I displayed minimum number using min function

---
+++ task2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+main()
+{
+  int num1,num2,result;
+  cout<<"Enter number1 and number2:";
+  cin>>num1>>num2;
+  result=min(num1,num2);
+  cout<<result<<" is minimum";
+}
+  
