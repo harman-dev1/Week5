adb132ec24959ac82fd23d742d704a647097c478
Harmain Iftikhar <120156793+harman-dev1@users.noreply.github.com>
2023-01-10 15:10:36 +0500

    In this program I displayed if user enter 40 then program display it in english or any other number from 1 to 100.

---
+++ task13.cpp
@@ -0,0 +1,124 @@
+#include<iostream>
+using namespace std;
+main(){
+    int number;
+    cout<<"Enter the number:";
+    cin>>number;
+    if(number==1){
+    cout<<"One";
+    }
+    if(number==2){
+        cout<<"Two";
+    }
+    if(number==3){
+        cout<<"Three";
+    }
+    if(number==4){
+        cout<<"Four";
+    }
+     if(number==5){
+        cout<<"Five";
+    }
+     if(number==6){
+        cout<<"Six";
+    }
+         if(number==7){
+        cout<<"Seven";
+    }
+         if(number==8){
+        cout<<"Eight";
+    }
+     if(number==9){
+        cout<<"Nine";
+    }
+    //  if(number==10){
+    //     cout<<"Ten";
+    // }
+    //  if(number==20){
+    //     cout<<"Twenty";
+    // }
+    //  if(number==30){
+    //     cout<<"Thirty";
+    // }
+    
+    //  if(number==40){
+    //     cout<<"Forty";
+    //  }
+     
+    //  if(number==50){
+    //     cout<<"fifty";
+    //  }
+    //  if(number==60){
+    //     cout<<"sixty";
+    //  }
+    //  if(number==70){
+    //     cout<<"seventy";
+    //  }
+    //  if(number==80){
+    //     cout<<"eighty";
+    //  }
+    //  if(number==90){
+    //     cout<<"ninety";
+    //  }
+
+     int a=number/10;
+     int b=number%10;
+     if(a==2){
+        cout<<"Twenty";
+    }
+     if(a==3){
+        cout<<"Thirty";
+    }
+    
+     if(a==4){
+        cout<<"Forty";
+     }
+     
+     if(a==5){
+        cout<<"fifty";
+     }
+     if(a==6){
+        cout<<"sixty";
+     }
+     if(a==7){
+        cout<<"seventy";
+     }
+     if(a==8){
+        cout<<"eighty";
+     }
+     if(a==9){
+        cout<<"ninety";
+     }
+    if(b != 0)
+    {
+        if(b==1){
+    cout<<"One";
+    }
+    if(b==2){
+        cout<<"Two";
+    }
+    if(b==3){
+        cout<<"Three";
+    }
+    if(b==4){
+        cout<<"Four";
+    }
+     if(b==5){
+        cout<<"Five";
+    }
+     if(b==6){
+        cout<<"Six";
+    }
+         if(b==7){
+        cout<<"Seven";
+    }
+         if(b==8){
+        cout<<"Eight";
+    }
+     if(b==9){
+        cout<<"Nine";
+    }
+ 
+    }
+       
+}
