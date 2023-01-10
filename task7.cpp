4b0ccf33c52a4149def8bb882b448cc3f7866e3f
Harmain Iftikhar <120156793+harman-dev1@users.noreply.github.com>
2023-01-10 15:00:20 +0500

    In this program I displayed a number after multiply with five by creating different   void function.

---
+++ task7.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+string isEvenish(int number);
+main()
+{
+  int number,sum,a,b,c,d,e;
+  cout<<"Enter number";
+  cin>>number;
+  a=number/10;
+  b=number%10;
+  cout<<a;}
+  
+  
